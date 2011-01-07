87e8935826bea7d6a944fcfd3e78f9610a8fc364
Hrimfaxi <outmatch@gmail.com>
2011-01-08 05:21:45 +0800

    Systemctrl add plain module patch. Vshctrl.prx in bootconf accepted and works.

--- SystemControl/module_handler.c
+++ SystemControl/module_handler.c
@@ -52,13 +52,41 @@ static int myKernelCreateThread(const char *name, void *entry, int pri, int stac
 
 void setup_module_handler(void)
 {
-	SceModule2 * mod;
-   
-	mod = (SceModule2 *)sceKernelFindModuleByName("sceModuleManager");
+	SceModule2 *mod;
+
+	mod = (SceModule2*) sceKernelFindModuleByName("sceModuleManager");
 
 	if (mod == NULL)
 		return;
+	
+	//psp go patches
+	if(psp_model == 4 ) {
+		//backup function pointer (dword_622C)
+		ProbeExec3 = (void*)mod->text_addr + 0x8864;
+
+		//override function (sub_0045C)
+		_sw(MAKE_CALL(_ProbeExec3), mod->text_addr + 0x7C6C);
+	}
+
+	_sw(MAKE_JUMP(_sceKernelCheckExecFile), mod->text_addr+0x87E4);
+
+	PartitionCheck = (void*)mod->text_addr + 0x7FE0;
+	_sw(MAKE_CALL(_PartitionCheck), mod->text_addr + 0x652C);
+	_sw(MAKE_CALL(_PartitionCheck), mod->text_addr + 0x68A8);
+
+	//no device check patches
+	_sw(0, mod->text_addr + 0x0760);
+	_sw(0x24020000, mod->text_addr + 0x07C0);
+	_sw(0, mod->text_addr + 0x30B0);
+	_sw(0, mod->text_addr + 0x310C);
+	_sw(0x10000009, mod->text_addr + 0x3138);
+	_sw(0, mod->text_addr + 0x3444);
+	_sw(0, mod->text_addr + 0x349C);
+	_sw(0x10000010, mod->text_addr + 0x34C8);
+	_sw(0, mod->text_addr + 0x4360);
+	_sw(0, mod->text_addr + 0x43A8);
+	_sw(0, mod->text_addr + 0x43C0);
 
 	hook_import_bynid((SceModule*)mod, "ThreadManForKernel", 0xF475845D, myKernelStartThread, 0);
 	hook_import_bynid((SceModule*)mod, "ThreadManForKernel", 0x446D8DE6, myKernelCreateThread, 0);
-}	
+}
--- SystemControl/syspatch.c
+++ SystemControl/syspatch.c
@@ -32,21 +32,6 @@ static int syspatch_module_chain(SceModule2 *mod)
 	return 0;
 }
 
-static void patch_sceLoaderCore(void)
-{
-	//find module
-	SceModule2 * loadcore = (SceModule2 *)sceKernelFindModuleByName("sceLoaderCore");
-
-	//undo rebootex patches
-	void * memlmd_3F2AC9C6 = (void*)sctrlHENFindFunction("sceMemlmd", "memlmd", 0x3F2AC9C6);
-	_sw(MAKE_CALL(memlmd_3F2AC9C6), loadcore->text_addr + 0x5CC8);
-	_sw(MAKE_CALL(memlmd_3F2AC9C6), loadcore->text_addr + 0x5CF8);
-	_sw(MAKE_CALL(memlmd_3F2AC9C6), loadcore->text_addr + 0x5D90);
-	void * memlmd_E42AFE2E = (void*)sctrlHENFindFunction("sceMemlmd", "memlmd", 0xE42AFE2E);
-	_sw(MAKE_CALL(memlmd_E42AFE2E), loadcore->text_addr + 0x41A4);
-	_sw(MAKE_CALL(memlmd_E42AFE2E), loadcore->text_addr + 0x5CA4);
-}
-
 void syspatch_init()
 {
 	setup_module_handler();

7be6e935d92ce8b62c111a9c774a54c0d992e620
Hrimfaxi <outmatch@gmail.com>
2011-01-09 14:33:37 +0800

    utils: add hexdump available in DEBUG, and marco NELEMS

--- Common/utils.c
+++ Common/utils.c
@@ -6,9 +6,30 @@
 #include <string.h>
 #include "systemctrl.h"
 #include "utils.h"
+#include "printk.h"
 
 void sync_cache(void)
 {
 	sceKernelIcacheInvalidateAll();
 	sceKernelDcacheWritebackInvalidateAll();
 }
+
+#ifdef DEBUG
+void hexdump(void *addr, int size)
+{
+	int i;
+	u8 *p = (u8*)addr;
+
+	if (addr == NULL) {
+		printk("hexdump: <NULL>\n");
+
+		return;
+	}
+
+	for(i=0; i<size; ++i) {
+		printk("%02X", p[i]);
+	}
+
+	printk("\n");
+}
+#endif
--- Common/utils.h
+++ Common/utils.h
@@ -4,8 +4,18 @@
 #define MAKE_JUMP(f) (0x08000000 | (((u32)(f) >> 2) & 0x03ffffff))
 #define MAKE_CALL(f) (0x0C000000 | (((u32)(f) >> 2) & 0x03ffffff)) 
 
+#define NELEMS(a) (sizeof(a) / sizeof(a[0]))
+
 int sceKernelGetModel(void);
 
 void sync_cache(void);
+
+#ifdef DEBUG
+void hexdump(void *addr, int size);
+#else
+static inline void hexdump(void *addr, int size)
+{
+}
+#endif
 
 #endif

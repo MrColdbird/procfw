bbc6cbbd1beaca6e5e162ef31d86ae08eed56e55
Hrimfaxi <outmatch@gmail.com>
2011-01-11 03:31:49 +0800

    Systemctrl: systemctrl_private add comment

--- Common/systemctrl_private.h
+++ Common/systemctrl_private.h
@@ -1,6 +1,8 @@
 #ifndef SYSTEMCTRL_PRIVATE_H
 #define SYSTEMCTRL_PRIVATE_H
 
+// Here goes the exported but unpublic functions
+
 int oe_mallocinit();
 void *oe_malloc(size_t size);
 void oe_free(void *p);

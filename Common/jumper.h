163955aded09b9fc167f7f47d61e2f7828a2578f
Hrimfaxi <outmatch@gmail.com>
2011-01-11 21:06:18 +0800

    jumper: add Coldbird's new version

--- Common/jumper.h
+++ Common/jumper.h
@@ -3,11 +3,17 @@
 
 #define GET_CALL_ADDR(i) ((i & 0x03ffffff) << 2)
 
-//maximum entry count
+//maximum entry count (2048 for user, 2048 for kernel)
 #define JUMPER_MAX_COUNT 2048
 
+//init user memory (only required if you intend to trace user modules)
+int initUserTraceMemory(void);
+
+//trace log function
+void dumpJAL(u32 target, u32 ra, u32 result);
+
 //single trace install
-int installJALTrace(u32 address);
+void installJALTrace(u32 address);
 
 //module trace install
 void installModuleJALTrace(SceModule2 * module);

fed855038a6a335cad0de6edb0cc76d2b9d3759c
coldbird <coldbird@localhost>
2011-01-12 21:37:37 +0100

    Removed useless double definition from printk.c. Fixed ISOLauncher UI Compilation Problem when in Debug Mode.

--- ISOLauncher/UI/main.c
+++ ISOLauncher/UI/main.c
@@ -268,7 +268,7 @@ int main(int argc, char* argv[])
 	g_conf.psp_model = kuKernelGetModel();
 	g_conf.iso_mode = USE_NP9660_MODE;
 
-	printk_init();
+	printk_init(NULL);
 	printk("PLoad GUI started, fw: 0x%08X model: 0x%08X\r\n", g_conf.fw_version, g_conf.psp_model);
 
 	{

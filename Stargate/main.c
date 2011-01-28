ecdafb821f5588a71f29286bbcd3637dcfed8e29
Hrimfaxi <outmatch@gmail.com>
2011-01-28 23:17:58 +0800

    Stargate: add nodrm support. allow to read plain EDATA or PGD files

--- Stargate/main.c
+++ Stargate/main.c
@@ -25,12 +25,16 @@
 PSP_MODULE_INFO("stargate", 0x1007, 1, 0);
 PSP_MAIN_THREAD_ATTR(0);
 
+extern int nodrm_init(void);
+extern void patch_drm_imports(SceModule *mod);
+
 static STMOD_HANDLER previous;
 
 static int stargate_module_chain(SceModule2 *mod)
 {
 	// for MHP3rd: a 6.36 game
 	hook_import_bynid((SceModule*)mod, "scePauth", 0x98B83B5D, myPauth_98B83B5D, 1);
+	patch_drm_imports((SceModule*)mod);
 	
 	if (previous)
 		return (*previous)(mod);
@@ -44,6 +48,7 @@ int module_start(SceSize args, void *argp)
 	printk("stargate started\n");
 	patch_sceMesgLed();
 	myPauth_init();
+	nodrm_init();
 	previous = sctrlHENSetStartModuleHandler(&stargate_module_chain);
 
 	return 0;

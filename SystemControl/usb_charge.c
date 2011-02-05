a0a49a8b1cc398bd43351836ecd9086334e3331a
Hrimfaxi <outmatch@gmail.com>
2011-02-05 20:26:02 +0800

    usb_charge: use vtimer now

--- SystemControl/usb_charge.c
+++ SystemControl/usb_charge.c
@@ -13,60 +13,69 @@
 #include "rebootex_conf.h"
 #include "libs.h"
 
-static int (*sceUsb_driver_AE5DE6AF)(char *, int, int) = NULL;
-static int (*sceUsb_driver_C21645A4)(void) = NULL;
-static int (*scePower_driver_E11A1999)(int) = NULL;
-static int (*scePower_driver_F3A08560)(int) = NULL;
-
-static int usb_charge_thread(SceSize argc, void *argp)
+static inline void *get_usb_driver_function(u32 nid)
 {
+	return (void*)sctrlHENFindFunction("sceUSB_Driver", "sceUsb_driver", nid);
+}
+
+static inline void *get_power_driver_function(u32 nid)
+{
+	return (void*)sctrlHENFindFunction("scePower_Service", "scePower_driver", nid);
+}
+
+static u32 usb_charge_timer_handler(SceUID uid, SceInt64 unk0, SceInt64 unk1, void *common)
+{
+	int (*_scePower_driver_E11A1999)(int) = NULL;
+	int (*_scePower_driver_F3A08560)(int) = NULL;
+	int (*_sceUsb_driver_AE5DE6AF)(char *, int, int) = NULL;
+	int (*_sceUsb_driver_C21645A4)(void) = NULL;
 	int ret;
-	int is_charging = 0;
+	static int is_charging = 0;
 
-	printk("Starting %s\n", __func__);
+	_sceUsb_driver_AE5DE6AF = get_usb_driver_function(0xAE5DE6AF);
+	_sceUsb_driver_C21645A4 = get_usb_driver_function(0xC21645A4);
+	_scePower_driver_F3A08560 = get_power_driver_function(0xF3A08560);
+	_scePower_driver_E11A1999 = get_power_driver_function(0xE11A1999);
 
-	while (1) {
-		sceUsb_driver_AE5DE6AF("USBBusDriver", 0, 0);
-		ret = sceUsb_driver_C21645A4() & 0x20;
+	if(_sceUsb_driver_AE5DE6AF == NULL ||
+			_sceUsb_driver_C21645A4 == NULL ||
+			_scePower_driver_F3A08560 == NULL ||
+			_scePower_driver_E11A1999 == NULL) {
+		return 2000000;
+	}
+	
+	(*_sceUsb_driver_AE5DE6AF)("USBBusDriver", 0, 0);
+	ret = (*_sceUsb_driver_C21645A4)() & 0x20;
 
-		if (is_charging == 1) {
-			if (ret == 0) {
-				printk("%s: stops charging\n", __func__);
-				scePower_driver_F3A08560(0);
-				is_charging = 0;
-			}
-		} else if (ret != 0) {
-			printk("%s: starts charging\n", __func__);
-			scePower_driver_E11A1999(1);
-			is_charging = 1;
-		}
-
-		sceKernelDelayThread(50000);
+	if (is_charging == 1 && ret == 0) {
+		printk("%s: stops charging\n", __func__);
+		(*_scePower_driver_F3A08560)(0);
+		is_charging = 0;
+	} else if (is_charging == 0 && ret != 0) {
+		printk("%s: starts charging\n", __func__);
+		(*_scePower_driver_E11A1999)(1);
+		is_charging = 1;
 	}
 
-	return 0;
+	return 5000000;
 }
 
 void usb_charge(void)
 {
-	int thid;
+	SceUID vtimer;
 
 	if (!conf.usbcharge || psp_model == PSP_1000 ) {
 		return;
 	}
 
-	sceUsb_driver_AE5DE6AF = (void*)sctrlHENFindFunction("sceUSB_Driver", "sceUsb_driver", 0xAE5DE6AF);
-	sceUsb_driver_C21645A4 = (void*)sctrlHENFindFunction("sceUSB_Driver", "sceUsb_driver", 0xC21645A4);
-	scePower_driver_E11A1999 = (void*)sctrlHENFindFunction("scePower_Service", "scePower_driver", 0xE11A1999);
-	scePower_driver_F3A08560 = (void*)sctrlHENFindFunction("scePower_Service", "scePower_driver", 0xF3A08560);
+	vtimer = sceKernelCreateVTimer("", NULL);
 
-	if (!sceUsb_driver_AE5DE6AF || !sceUsb_driver_C21645A4 || !scePower_driver_E11A1999 || !scePower_driver_F3A08560) {
+	if(vtimer < 0) {
+		printk("%s: sceKernelCreateVTimer -> 0x%08X\n", __func__, vtimer);
+
 		return;
 	}
 
-	thid = sceKernelCreateThread("UsbCharger", &usb_charge_thread, 32, 0x800, 0, 0);
-
-	if (thid >= 0) {
-		sceKernelStartThread(thid, 0, NULL);
-	}
+	sceKernelStartVTimer(vtimer);
+	sceKernelSetVTimerHandlerWide(vtimer, 5000000, usb_charge_timer_handler, NULL);
 }

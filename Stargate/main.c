bbd85c26474425f85b871fc8cee26a545e82cf30
Hrimfaxi <outmatch@gmail.com>
2011-02-07 00:52:51 +0800

    nodrm: fix bug in m33 with patching weak imports when npdrm loaded

--- Stargate/main.c
+++ Stargate/main.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "systemctrl.h"
+#include "systemctrl_private.h"
 #include "kubridge.h"
 #include "utils.h"
 #include "printk.h"
@@ -27,16 +28,72 @@ PSP_MAIN_THREAD_ATTR(0);
 
 static STMOD_HANDLER previous;
 
+#define MAX_MODULE_NUMBER 256
+
+static void fix_weak_imports(void)
+{
+	SceUID *modids;
+	int ret;
+	int i, count;
+	int k1;
+
+	k1 = pspSdkGetK1();
+	pspSdkSetK1(0);
+	modids = oe_malloc(MAX_MODULE_NUMBER * sizeof(SceUID));
+
+	if(modids == NULL) {
+		printk("%s: allocating modids failed\n", __func__);
+		goto exit;
+	}
+
+	memset(modids, 0, MAX_MODULE_NUMBER * sizeof(SceUID));
+	ret = sceKernelGetModuleIdList(modids, MAX_MODULE_NUMBER * sizeof(SceUID), &count);
+	
+	if (ret < 0) {
+		printk("%s: sceKernelGetThreadmanIdList -> 0x%08x\n", __func__, ret);
+		oe_free(modids);
+		goto exit;
+	}
+
+	for(i=0; i<count; ++i) {
+		SceModule2 *pMod;
+
+		pMod = (SceModule2*)sceKernelFindModuleByUID(modids[i]);
+
+		if (pMod != NULL && (pMod->attribute & 0x1000) == 0) {
+			patch_drm_imports((SceModule*)pMod);
+		}
+	}
+
+	oe_free(modids);
+exit:
+	pspSdkSetK1(k1);
+}
+
 static int stargate_module_chain(SceModule2 *mod)
 {
+	if (previous)
+		(*previous)(mod);
+
 	// for MHP3rd: a 6.36 game
 	hook_import_bynid((SceModule*)mod, "scePauth", 0x98B83B5D, myPauth_98B83B5D, 1);
 	patch_drm_imports((SceModule*)mod);
 	patch_utility((SceModule*)mod);
-	
-	if (previous)
-		return (*previous)(mod);
 
+	// m33 mode: until npdrm loaded
+	if(0 == strcmp(mod->modname, "scePspNpDrm_Driver")) {
+		int ret;
+
+		fix_weak_imports();
+		ret = nodrm_get_npdrm_functions();
+
+#ifdef DEBUG
+		if (ret < 0) {
+			printk("%s: nodrm_get_npdrm_functions -> %d\n", __func__, ret);
+		}
+#endif
+	}
+	
 	return 0;
 }
 
@@ -47,6 +104,8 @@ int module_start(SceSize args, void *argp)
 	patch_sceMesgLed();
 	myPauth_init();
 	nodrm_init();
+	nodrm_get_normal_functions();
+	nodrm_get_npdrm_functions(); // np9660 mode: npdrm already loaded
 	previous = sctrlHENSetStartModuleHandler(&stargate_module_chain);
 
 	return 0;

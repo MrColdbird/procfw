9f6fe9d49e1f1247f5930f4324ebcdf9ec5e94e9
Hrimfaxi <outmatch@gmail.com>
2011-02-08 01:31:53 +0800

    mediasync: patch DiscCheckMedia on UMD units. To avoid ISO drivers fail on UMD inserted

--- SystemControl/mediasync_patch.c
+++ SystemControl/mediasync_patch.c
@@ -15,4 +15,8 @@ void patch_sceMediaSync(u32 scemediasync_text_addr)
 	// Let it return 1 always
 	_sw(0x03E00008, scemediasync_text_addr+0x744);
 	_sw(0x24020001, scemediasync_text_addr+0x748);
+
+	// patch DiscCheckMedia
+	_sw(0x1000001D, scemediasync_text_addr+0x3C4);
+	_sw(0x1000001D, scemediasync_text_addr+0xDC8);
 }

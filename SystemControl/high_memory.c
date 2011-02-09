84d425c92b96d101d914a31e0d929804370f91e3
Hrimfaxi <outmatch@gmail.com>
2011-02-10 00:58:48 +0800

    high_memory: Resevered P11 size to avoid crash on standby.

--- SystemControl/high_memory.c
+++ SystemControl/high_memory.c
@@ -13,11 +13,14 @@
 
 typedef struct _MemPart {
 	u32 *meminfo;
-	void *addr;
-	u32 size;
+	int offset;
+	int size;
 } MemPart;
 
 int g_high_memory_enabled = 0;
+
+//in system memory manager
+static unsigned int * (*get_partition)(int pid) = (void *)(0x88003E34);
 
 //prevent umd-cache in homebrew, so we can drain the cache partition.
 void patch_umdcache(u32 text_addr)
@@ -82,9 +85,20 @@ static int display_meminfo(void)
 	return 0;
 }
 
-static void modify_partition(u32 *meminfo, u32 offset, u32 size)
+static void modify_partition(MemPart *part)
 {
-	if (offset != (u32)-1) {
+	u32 *meminfo;
+	u32 offset, size;
+
+	meminfo = part->meminfo;
+	offset = part->offset;
+	size = part->size;
+
+	if(meminfo == NULL) {
+		return;
+	}
+
+	if (offset != 0) {
 		meminfo[1] = (offset << 20) + 0x88800000;
 	}
 
@@ -92,50 +106,50 @@ static void modify_partition(u32 *meminfo, u32 offset, u32 size)
 	((u32*)(meminfo[4]))[5] = (size << 21) | 0xFC;
 }
 
+static void delete_part_11(void)
+{
+	// P11 pointer in sysmem+0x00003EF8
+	_sw(0, 0x880145e8);
+}
+
 void prepatch_partitions(void)
 {
-	unsigned int * (*GetPartition)(int pid) = (void *)(0x88003E34);
 	MemPart p8, p11;
-	u32 total_user_size;
+	int apitype;
 
-	if(g_p2_size == 24 && g_p9_size == 24) {
-		total_user_size = MAX_HIGH_MEMSIZE;
-	} else {
-		total_user_size = g_p2_size + g_p9_size;
+	apitype = sceKernelInitApitype();
+
+	if(apitype == 0x152 || apitype == 0x141) {
+		delete_part_11();
 	}
 
-	p8.meminfo = (*GetPartition)(8);
-	p8.size = MIN(4, 56 - total_user_size);
-
-	p11.meminfo = (*GetPartition)(11);
-	p11.size = 0; // nuke p11
-
-	if (p8.meminfo != NULL) {
-		// move p8 to total_user_size
-		modify_partition(p8.meminfo, total_user_size, p8.size);
+	p8.meminfo = (*get_partition)(8);
+	p11.meminfo = (*get_partition)(11);
+	
+	p8.size = 1;
+	
+	if(p11.meminfo != NULL) {
+		p8.offset = 56-4-1;
 	} else {
-		printk("%s: part8 not found\n", __func__);
+		p8.offset = 56-1;
 	}
 
-	if (p11.meminfo != NULL) {
-		// move p11 to the end of p8
-		modify_partition(p11.meminfo, (total_user_size + p8.size), p11.size);
-	} else {
-//		printk("%s: part11 not found\n", __func__);
-	}
+	modify_partition(&p8);
+
+	p11.size = 4;
+	p11.offset = 56-4;
+	modify_partition(&p11);
 }
 
 void patch_partitions(void) 
 {
 	MemPart p2, p9;
-	//system memory manager
-	unsigned int * (*GetPartition)(int pid) = (void *)(0x88003E34);
 
 	// shut up gcc warning
 	(void)display_meminfo;
 
-	p2.meminfo = (*GetPartition)(2);
-	p9.meminfo = (*GetPartition)(9);
+	p2.meminfo = (*get_partition)(2);
+	p9.meminfo = (*get_partition)(9);
 
 	if(g_p2_size == 24 && g_p9_size == 24) {
 		p2.size = MAX_HIGH_MEMSIZE;
@@ -145,24 +159,29 @@ void patch_partitions(void)
 		p9.size = g_p9_size;
 	}
 
+	if ((*get_partition)(11) != NULL && p2.size + p9.size > 56 - 1 - 4) {
+		// reserved 4MB for P11
+		int reserved_len;
+
+		reserved_len = p2.size + p9.size - (56 - 1 - 4);
+
+		if(p9.size > reserved_len) {
+			p9.size -= reserved_len;
+		} else {
+			p2.size -= reserved_len; 
+		}
+	}
+
 	printk("%s: p2/p9 %d/%d\n", __func__, p2.size, p9.size);
 
 	//reset partition length for next reboot
 	sctrlHENSetMemory(24, 24);
 
-	if(p2.meminfo != NULL) {
-		//resize partition 2
-		modify_partition(p2.meminfo, (u32)-1, p2.size);
-	} else {
-		printk("%s: part2 not found\n", __func__);
-	}
+	p2.offset = 0;
+	modify_partition(&p2);
 
-	if (p9.meminfo != NULL) {
-		// at end of p2
-		modify_partition(p9.meminfo, p2.size, p9.size);
-	} else {
-		printk("%s: part9 not found\n", __func__);
-	}
+	p9.offset = p2.size;
+	modify_partition(&p9);
 
 	g_high_memory_enabled = 1;
 	unlock_high_memory(0);

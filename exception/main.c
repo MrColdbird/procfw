59adfb2dd0ee368c03b1f7d9eda75eccd8216245
Hrimfaxi <outmatch@gmail.com>
2011-02-23 21:07:18 +0800

    exception: Now working.

--- exception/main.c
+++ exception/main.c
@@ -10,17 +10,18 @@
 #include "strsafe.h"
 #include "printk.h"
 #include "systemctrl.h"
+#include "utils.h"
 
 #define MAX_BACKTRACE_NUM 10
 
 PSP_MODULE_INFO("exception", 0x1007, 1, 1);
 PSP_MAIN_THREAD_ATTR(0);
 
+extern u8 myDebugExceptionHandler;
+extern int psplinkPresent(void);
+
 PspDebugErrorHandler curr_handler;
 PspDebugRegBlock exception_regs;
-
-extern void *myDebugExceptionHandler;
-extern int psplinkPresent(void);
 
 static const char *codeTxt[32] = {
 	"Interrupt", "TLB modification", "TLB load/inst fetch", "TLB store",
@@ -42,15 +43,15 @@ static const unsigned char regName[32][5] = {
 
 void ExceptionHandler(PspDebugRegBlock * regs)
 {
-#if 1
 	int i, found;
 	PspDebugStackTrace traces[MAX_BACKTRACE_NUM];
-	char testo[512];
 	char timestr[80];
 	pspTime tm;
 	SceModule2 *mod;
 	const char *modname;
 	u32 mod_textaddr;
+
+	fill_vram(0x00FF0000);
 
 	mod = (SceModule2*)sceKernelFindModuleByAddress(regs->epc);
 
@@ -62,23 +63,18 @@ void ExceptionHandler(PspDebugRegBlock * regs)
 		mod_textaddr = 0x00000000;
 	}
 
-	printk("%-21s: %08X\n", "PSP firmware version",
-			sceKernelDevkitVersion());
-	printk("%-21s: 0%dg\n", "PSP type", sceKernelGetModel());
+	printk("%-21s: %08X\n", "PSP firmware version", sceKernelDevkitVersion());
+	printk("%-21s: 0%dg\n", "PSP type", sceKernelGetModel()+1);
 	sceRtcGetCurrentClockLocalTime(&tm);
-	sprintf(timestr, "%u-%u-%u %02u:%02u:%02u", tm.year, tm.month,
-			tm.day, tm.hour, tm.minutes, tm.seconds);
-
+	sprintf(timestr, "%u-%u-%u %02u:%02u:%02u", tm.year, tm.month, tm.day, tm.hour, tm.minutes, tm.seconds);
 	printk("%-21s: %s\n", "Crash time", timestr);
+	printk("%-21s: 0x%03X\n", "Api type", sceKernelInitApitype());
 
 	printk("\n");
 
 	printk("Exception details:\n\n");
-	printk("Exception - %s\n",
-			codeTxt[(regs->cause >> 2) & 31]);
-	printk("EPC       - %08X / %s.text + %08X\n",
-			(int) regs->epc, modname,
-			(unsigned int) (regs->epc - mod_textaddr));
+	printk("Exception - %s\n", codeTxt[(regs->cause >> 2) & 31]);
+	printk("EPC       - %08X / %s+0x%08X\n", (int) regs->epc, modname, (u32) (regs->epc - mod_textaddr));
 	printk("Cause     - %08X\n", (int) regs->cause);
 	printk("Status    - %08X\n", (int) regs->status);
 	printk("BadVAddr  - %08X\n", (int) regs->badvaddr);
@@ -86,9 +82,9 @@ void ExceptionHandler(PspDebugRegBlock * regs)
 	for (i = 0; i < 32; i += 4) {
 		printk("%s:%08X %s:%08X %s:%08X %s:%08X\n",
 				regName[i], (int) regs->r[i], regName[i + 1],
-				(int) regs->r[i + 1], regName[i + 2],
-				(int) regs->r[i + 2], regName[i + 3],
-				(int) regs->r[i + 3]);
+				regs->r[i + 1], regName[i + 2],
+				regs->r[i + 2], regName[i + 3],
+				regs->r[i + 3]);
 	}
 
 	printk("\n");
@@ -98,19 +94,33 @@ void ExceptionHandler(PspDebugRegBlock * regs)
 	printk("Call Trace:\n");
 
 	for (i = 0; i < found; ++i) {
-		sprintf(testo,
-				"\t%d: caller %08X(%08X) func %08X(%08X)\n", i,
-				(unsigned int) traces[i].call_addr,
-				(unsigned int) traces[i].call_addr - mod_textaddr,
-				(unsigned int) traces[i].func_addr,
-				(unsigned int) traces[i].func_addr - mod_textaddr);
+		printk("\t%d: caller %08X(%08X) func %08X(%08X)\n", i,
+				(u32) traces[i].call_addr,
+				(u32) traces[i].call_addr - mod_textaddr,
+				(u32) traces[i].func_addr,
+				(u32) traces[i].func_addr - mod_textaddr);
 	}
 
 	printk_sync();
 
-	sceKernelDelayThread(100000);
-#endif
+	sceKernelDelayThread(500000);
 	sctrlKernelExitVSH(NULL);
+
+	while(1) {
+		sceKernelDelayThread(100000);;
+	}
+}
+
+static int crash_thread(SceSize args, void *argp)
+{
+	sceKernelDelayThread(10 * 1000000L);
+	printk("%s: crashing...\n", __func__);
+
+	while (1) {
+		_sw(0, 0);
+	}
+
+	return 0;
 }
 
 int module_start(SceSize args, void *argp)
@@ -122,10 +132,16 @@ int module_start(SceSize args, void *argp)
 	}
 
 	printk_init("ms0:/exception.txt");
-	printk("exception started\n");
 	curr_handler = ExceptionHandler;
-	ret = sceKernelRegisterDefaultExceptionHandler(myDebugExceptionHandler);
-	printk("%s: sceKernelRegisterDefaultExceptionHandler -> 0x%08X\n", __func__, ret);
+	ret = sceKernelRegisterDefaultExceptionHandler(&myDebugExceptionHandler);
+
+	if( 0 ) {
+		int thid;
+
+		thid = sceKernelCreateThread("crash_thread", &crash_thread, 0x10, 0x1000, 0, 0);
+
+		sceKernelStartThread(thid, 0, NULL);
+	}
 
 	return 0;
 }

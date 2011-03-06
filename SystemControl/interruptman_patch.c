2379f253060ba4e6a0097e32c774a87826b6c501
hrimfaxi <outmatch@gmail.com>
2011-03-06 10:28:05 +0800

    interruptman_patch: added comments and TODO

--- SystemControl/interruptman_patch.c
+++ SystemControl/interruptman_patch.c
@@ -13,8 +13,14 @@ void patch_sceInterruptManager(void)
 {
 	SceModule2 *mod = (SceModule2*) sceKernelFindModuleByName("sceInterruptManager");
 
-	_sw(0, mod->text_addr + 0xDE8);
-	_sw(0, mod->text_addr + 0xDEC);
-	_sw(0, mod->text_addr + 0xE94);
-	_sw(0, mod->text_addr + 0xE98);
+	// disable writing invalid address to reset vector
+	_sw(0, mod->text_addr + 0x00000DE8);
+	_sw(0, mod->text_addr + 0x00000DEC);
+
+	// disable crash code
+	_sw(0, mod->text_addr + 0x00000E94);
+	_sw(0, mod->text_addr + 0x00000E98);
+
+	//TODO currently syscall on kernel module patch is not really working
+	//the syscall call on kernel module would always do nothing but return 0
 }

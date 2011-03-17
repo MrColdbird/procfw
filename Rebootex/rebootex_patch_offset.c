9af8799a8554fb5dcc384835586c3e2a698e6470
hrimfaxi <outmatch@gmail.com>
2011-03-17 21:22:34 +0800

    Rebootex: added rebootex_patch_offset

---
+++ Rebootex/rebootex_patch_offset.c
@@ -0,0 +1,24 @@
+#include <pspsdk.h>
+#include "rebootex_patch_offset.h"
+
+PatchOffset g_635_offsets = {
+	.fw_version = 0x06030510,
+	.loadexec_patch_05g = {
+		.LoadRebootCall = 0x00002F90,
+		.RebootJump = 0x00002FDC,
+	},
+	.loadexec_patch_other = {
+		.LoadRebootCall = 0x00002D44,
+		.RebootJump = 0x00002D90,
+	},
+	.sceKernelGetModel = 0x88000000 + 0x0000A13C,
+};
+
+PatchOffset *g_offs = NULL;
+
+void setup_patch_offset_table(u32 fw_version)
+{
+	if(fw_version == g_635_offsets.fw_version) {
+		g_offs = &g_635_offsets;
+	}
+}
---
+++ Rebootex/rebootex_patch_offset.h
@@ -0,0 +1,20 @@
+#ifndef REBOOTEX_PATCH_OFFSET_H
+#define REBOOTEX_PATCH_OFFSET_H
+
+struct LoadExecPatch {
+	u32 LoadRebootCall;
+	u32 RebootJump;
+};
+
+typedef struct _PatchOffset {
+	u32 fw_version;
+	struct LoadExecPatch loadexec_patch_05g;
+	struct LoadExecPatch loadexec_patch_other;
+	u32 sceKernelGetModel;
+} PatchOffset;
+
+extern PatchOffset *g_offs;
+
+void setup_patch_offset_table(u32 fw_version);
+
+#endif

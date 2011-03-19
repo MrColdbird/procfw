dcc700d34c77166df3e8abc84c710a87f8e158e2
hrimfaxi <outmatch@gmail.com>
2011-03-20 02:15:12 +0800

    Stargate: added stargate_patch_offset for 6.20

--- Stargate/stargate_patch_offset.c
+++ Stargate/stargate_patch_offset.c
@@ -27,11 +27,39 @@ PatchOffset g_635_offsets = {
 	},
 };
 
+PatchOffset g_620_offsets = {
+	.fw_version = FW_620,
+	.sceMesgLedDecryptGame1 = {
+		0x000028EC, // 01g
+		0x00002BD4, // 02g
+		0x00002E9C, // 03g
+		0x00002E9C, // 04g
+		0x00003128, // 05g
+		0xDEADBEEF, // 06g
+		0xDEADBEEF, // 07g
+		0xDEADBEEF, // 08g
+		0xDEADBEEF, // 09g
+	},
+	.sceMesgLedDecryptGame2 = {
+		0x00002CA0, // 01g
+		0x00002FD0, // 02g
+		0x000032E0, // 03g
+		0x000032E0, // 04g
+		0x000035B4, // 05g
+		0xDEADBEEF, // 06g
+		0xDEADBEEF, // 07g
+		0xDEADBEEF, // 08g
+		0xDEADBEEF, // 09g
+	},
+};
+
 PatchOffset *g_offs = NULL;
 
 void setup_patch_offset_table(u32 fw_version)
 {
 	if(fw_version == g_635_offsets.fw_version) {
 		g_offs = &g_635_offsets;
+	} else if(fw_version == g_620_offsets.fw_version) {
+		g_offs = &g_620_offsets;
 	}
 }

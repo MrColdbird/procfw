f98f793adb8f602c5e000bcf053d374269cf2fea
hrimfaxi <outmatch@gmail.com>
2011-03-19 17:09:14 +0800

    PXELauncher: added patch_offset and 620 support

---
+++ PXE/Launcher/launcher_patch_offset.h
@@ -0,0 +1,26 @@
+#ifndef LAUNCHER_PATCH_OFFSET_H
+#define LAUNCHER_PATCH_OFFSET_H
+
+struct sceLoadExecPatch {
+	u32 LoadReboot;
+	u32 LoadRebootCall;
+	u32 RebootAddress;
+};
+
+typedef struct _PatchOffset {
+	u32 fw_version;
+	u32 sceKernelIcacheInvalidateAll;
+	u32 sceKernelDcacheWritebackInvalidateAll;
+	u32 sceKernelFindModuleByName;
+	u32 sceKernelGetModel;
+	struct sceLoadExecPatch loadexec_patch_05g;
+	struct sceLoadExecPatch loadexec_patch_other;
+	u32 patchRangeStart;
+	u32 patchRangeEnd;
+} PatchOffset;
+
+extern PatchOffset *g_offs;
+
+void setup_patch_offset_table(u32 fw_version);
+
+#endif

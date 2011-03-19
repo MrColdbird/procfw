f910b9d7adfcf6bd65c6f505904c8f0059ff59aa
hrimfaxi <outmatch@gmail.com>
2011-03-19 17:40:02 +0800

    Rebootex: added 620 offsets

--- Rebootex/rebootex_patch_offset.c
+++ Rebootex/rebootex_patch_offset.c
@@ -13,11 +13,25 @@ PatchOffset g_635_offsets = {
 	},
 };
 
+PatchOffset g_620_offsets = {
+	.fw_version = FW_620,
+	.loadexec_patch_05g = {
+		.LoadRebootCall = 0x00002F28,
+		.RebootJump = 0x00002F74,
+	},
+	.loadexec_patch_other = {
+		.LoadRebootCall = 0x00002CD8,
+		.RebootJump = 0x00002D24,
+	},
+};
+
 PatchOffset *g_offs = NULL;
 
 void setup_patch_offset_table(u32 fw_version)
 {
 	if(fw_version == g_635_offsets.fw_version) {
 		g_offs = &g_635_offsets;
+	} else if(fw_version == g_620_offsets.fw_version) {
+		g_offs = &g_620_offsets;
 	}
 }

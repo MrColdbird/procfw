148725261b5eee222c7e28b3e39b8335472000f3
Hrimfaxi <outmatch@gmail.com>
2011-03-27 14:36:47 +0800

    VersionSpoofer: added version.txt valid check

--- SystemControl/version_spoof.c
+++ SystemControl/version_spoof.c
@@ -14,33 +14,90 @@
 
 int sceResmgr_driver_9DC14891(u8 *buf, u32 size, u32* newsize);
 
-int versionspoofer(u8 *buf, u32 size, u32* newsize)
+static const char *g_ver_checklist[] = {
+	"release:",
+	"build:",
+	"system:",
+	"vsh:",
+	"target:",
+};
+
+static int load_version_txt(void *buf, int size)
 {
-	//grab file contents
-	int result = sceResmgr_driver_9DC14891(buf, size, newsize);
+	SceUID fd;
 
-	//version.txt spotted!
-	if(!rebootex_conf.recovery_mode && conf.useversion && strstr((const char*)buf, "release:")) {
-		//increase permission level
-		u32 k1 = pspSdkSetK1(0);
+   	fd = sceIoOpen("ms0:/seplugins/version.txt", PSP_O_RDONLY, 0777);
 
-		//open version.txt
-		SceUID fd = sceIoOpen("ms0:/seplugins/version.txt", PSP_O_RDONLY, 0777);
-		if(fd < 0) fd = sceIoOpen("ef0:/seplugins/version.txt", PSP_O_RDONLY, 0777);
+	if(fd < 0) {
+		fd = sceIoOpen("ef0:/seplugins/version.txt", PSP_O_RDONLY, 0777);
+	}
 
-		if(fd >= 0) {
-			//read contents
-			*newsize = sceIoRead(fd, buf, size);
+	if(fd < 0) {
+		return fd;
+	}
 
-			//close file
-			sceIoClose(fd);
+	size = sceIoRead(fd, buf, size);
+	sceIoClose(fd);
+
+	return size;
+}
+
+static int check_valid_version_txt(const void *buf, int size)
+{
+	const char *p;
+	int i;
+
+	p = buf;
+
+	if(size < 159) {
+		return -1;
+	}
+
+	for(i=0; i<NELEMS(g_ver_checklist); ++i) {
+		if(p - (const char*)buf >= size) {
+			return -2;
 		}
 
-		//restore permission level
+		if(0 != strncmp(p, g_ver_checklist[i], strlen(g_ver_checklist[i]))) {
+			return -3;
+		}
+
+		while(*p != '\n' && p - (const char*)buf < size) {
+			p++;
+		}
+
+		if(p - (const char*)buf >= size) {
+			return -4;
+		}
+
+		p++;
+	}
+
+	return 0;
+}
+
+int versionspoofer(u8 *buf, u32 size, u32* newsize)
+{
+	int result = sceResmgr_driver_9DC14891(buf, size, newsize);
+
+	if(!rebootex_conf.recovery_mode && conf.useversion && strstr((const char*)buf, "release:")) {
+		char tmpbuf[159];
+		int tmpsize;
+		u32 k1;
+
+		k1 = pspSdkSetK1(0);
+		tmpsize = load_version_txt(tmpbuf, sizeof(tmpbuf));
+
+		if(tmpsize > 0 && check_valid_version_txt(tmpbuf, tmpsize) == 0) {
+			printk("%s: custom version.txt loaded\n", __func__);
+			memcpy(buf, tmpbuf, tmpsize);
+			*newsize = tmpsize;
+			result = 0;
+		}
+
 		pspSdkSetK1(k1);
 	}
 
-	//return result
 	return result;
 }
 

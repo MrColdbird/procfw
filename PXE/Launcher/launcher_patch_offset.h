b07334244cd09dc08af60fdc7ea5de3bd34e7192
Hrimfaxi <outmatch@gmail.com>
2011-06-03 02:44:04 +0800

    chn_iso: some refactor

---
+++ PXE/Launcher/launcher_patch_offset.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of PRO CFW.
+
+ * PRO CFW is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * PRO CFW is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PRO CFW. If not, see <http://www.gnu.org/licenses/ .
+ */
+
+#ifndef LAUNCHER_PATCH_OFFSET_H
+#define LAUNCHER_PATCH_OFFSET_H
+
+#include "utils.h"
+
+struct sceLoadExecPatch {
+	u32 LoadReboot;
+	u32 LoadRebootCall;
+	u32 RebootJump;
+};
+
+typedef struct _PatchOffset {
+	u32 fw_version;
+	u32 sceKernelIcacheInvalidateAll;
+	u32 sceKernelDcacheWritebackInvalidateAll;
+	u32 sceKernelFindModuleByName;
+	u32 sceKernelGetModel;
+	struct sceLoadExecPatch loadexec_patch_05g;
+	struct sceLoadExecPatch loadexec_patch_other;
+	u32 sceKernelPowerLockForUser;
+	u16 sceKernelPowerLockForUser_data_offset;
+	u32 patchRangeStart;
+	u32 patchRangeEnd;
+} PatchOffset;
+
+extern PatchOffset *g_offs;
+
+void setup_patch_offset_table(u32 fw_version);
+
+#endif
---
+++ exception/README.txt
@@ -0,0 +1,4 @@
+Exception: A optional plugins that can catch most of exceptions and write to ms0:/exception.txt.
+Enable it in seplugins and run your application or game or pops.
+
+Note: Not all exceptions(crash) can be caught.
